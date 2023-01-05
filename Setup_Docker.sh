de17a180d859287eed33ee7259e6b04df34e799b
Muhammad Fikri <fikri3142857@gmail.com>
2023-01-05 19:26:41 +0700

    create setup_docker

--- Setup_Docker.sh
+++ Setup_Docker.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
 sudo docker build -t "webserver:latest" .
+sudo docker image prune
+sudo docker container rm -f APIServer
+sudo docker container rm -f WebServer
 sudo docker run --detach --publish 8081:8081 --name APIServer --restart always webserver:latest /bin/sh -c 'cd /WebServer; python3 API_Server.py'
 sudo docker run --detach --publish 80:80 --name WebServer --restart always webserver:latest /bin/sh -c 'cd /WebServer; python3 Web_Server.py'
